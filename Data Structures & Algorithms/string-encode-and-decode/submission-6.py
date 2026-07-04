07dee5a3562d53ab104686a6f396a98e613dae1b
LaCat69 <sergiopanastony@gmail.com>
2026-07-04 14:59:20 +0200

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for char in strs:
+            result += f"{len(char)}" + "#" + char
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        result = []
+        while i < len(s):
+            gap = ""
+            while s[i].isdigit():
+                gap += s[i]
+                i += 1
+            i += 1
+            int_gap = int(gap)
+            result.append(s[i:int_gap+i])
+            i += int(gap)
+        return result
