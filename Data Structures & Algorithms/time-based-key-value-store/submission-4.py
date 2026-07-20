27dc773a81d09194067b1e690830973675e60ca3
LaCat69 <sergiopanastony@gmail.com>
2026-07-20 12:21:33 +0200

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dictonary = defaultdict(list)    
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dictonary[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not key in self.dictonary:
+            return ""
+        arr = self.dictonary[key]
+        l, r = 0, len(arr)-1
+
+        while l <= r:
+            m = (l + r) // 2
+            guess = arr[m]
+
+            if timestamp == guess[0]:
+                return guess[1]
+            elif timestamp > guess[0]:
+                l = m + 1
+            else:
+                r = m - 1
+        return arr[r][1] if r >= 0 and arr[r][0] < timestamp else ""
+        
